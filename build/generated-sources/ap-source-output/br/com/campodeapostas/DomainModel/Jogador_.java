761a0a52ce6af94ebbe764da1009c5f8ab63b91f
Nayara Borges <naborgesdf@hotmail.com>
2013-10-22 20:17:45 -0200

    correçoes

--- build/generated-sources/ap-source-output/br/com/campodeapostas/DomainModel/Jogador_.java
+++ build/generated-sources/ap-source-output/br/com/campodeapostas/DomainModel/Jogador_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-10-22T00:36:58")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-10-22T20:04:17")
 @StaticMetamodel(Jogador.class)
 public class Jogador_ { 
 
